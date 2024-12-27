91319651b3bee83c62ef0ec61f1af597e0f3c44b
Groove Salad <95263024+Priscillalala@users.noreply.github.com>
2024-12-27 14:18:53 -0800

    good browser jumpscare

--- Editor/Browser/AddressableBrowser.cs
+++ Editor/Browser/AddressableBrowser.cs
@@ -31,17 +31,31 @@ namespace AssetsOfRain.Editor.Browser
         [Flags]
         public enum BrowserOptionsExtended
         {
-            None = 0x0,
-            ShowType = 0x1,
-            ShowProvider = 0x2,
-            IgnoreCase = 0x4,
-            UseRegex = 0x8,
-            OnlyImportedAssets = 0x16,
+            None,
+            ShowType = 1 << 0,
+            ShowProvider = 1 << 1,
+            IgnoreCase = 1 << 2,
+            UseRegex = 1 << 3,
+            InProject = 1 << 4,
+            Available = 1 << 5,
+            NewOptions = InProject | Available,
+        }
+
+        public enum AssetStatus
+        {
+            NeverAvailable,
+            InProject,
+            Available,
+            Neutral,
         }
 
         private static readonly FieldInfo directoryContentField = typeof(ThunderKit.Addressable.Tools.AddressableBrowser).GetField("directoryContent", BindingFlags.Instance | BindingFlags.NonPublic);
+        private static readonly FieldInfo directoryField = typeof(ThunderKit.Addressable.Tools.AddressableBrowser).GetField("directory", BindingFlags.Instance | BindingFlags.NonPublic);
         private static readonly MethodInfo DirectoryContent_onSelectionChanged = typeof(ThunderKit.Addressable.Tools.AddressableBrowser).GetMethod("DirectoryContent_onSelectionChanged", BindingFlags.Instance | BindingFlags.NonPublic);
+        private static readonly MethodInfo GroupLocation = typeof(ThunderKit.Addressable.Tools.AddressableBrowser).GetMethod("GroupLocation", BindingFlags.Instance | BindingFlags.NonPublic);
+        private static readonly FieldInfo DirectoryContentsField = typeof(ThunderKit.Addressable.Tools.AddressableBrowser).GetField("DirectoryContents", BindingFlags.Instance | BindingFlags.NonPublic);
         private static readonly FieldInfo m_CallbackRegistry = typeof(CallbackEventHandler).GetField("m_CallbackRegistry", BindingFlags.Instance | BindingFlags.NonPublic);
+        
         public override string Title => "Addressable Browser+";
 
         const string ADDRESSABLE_ASSET = "addressable-asset";
@@ -50,6 +64,7 @@ namespace AssetsOfRain.Editor.Browser
         const string DISPLAY_OPTIONS = "display-options";
 
         public BrowserOptionsExtended browserOptionsExtended;
+        private Dictionary<string, List<IResourceLocation>> FullDirectoryContents;
 
         public override void OnEnable()
         {
@@ -72,36 +87,29 @@ namespace AssetsOfRain.Editor.Browser
         {
             Debug.Log("Modify browser");
             ListView directoryContent = GetDirectoryContent();
+
             directoryContent.bindItem = (Action<VisualElement, int>)Delegate.Combine(directoryContent.bindItem, new Action<VisualElement, int>(ModifyAsset));
+            
             var onSelectionChanged = (Action<IEnumerable<object>>)Delegate.CreateDelegate(typeof(Action<IEnumerable<object>>), this, DirectoryContent_onSelectionChanged);
             directoryContent.onSelectionChange -= onSelectionChanged;
             directoryContent.onItemsChosen += onSelectionChanged;
 
-            /*var displayOptionsField = rootVisualElement.Q<EnumFlagsField>(DISPLAY_OPTIONS);
-            displayOptionsField.Unbind();
-            displayOptionsField.style.color = Color.red;
-            Debug.Log(displayOptionsField.bindingPath);
-            displayOptionsField.bindingPath = nameof(browserOptionsExtended);
-            Debug.Log(displayOptionsField.bindingPath);
-            displayOptionsField.BindProperty(new SerializedObject(this).FindProperty(nameof(browserOptionsExtended)));
-            displayOptionsField.Init(BrowserOptionsExtended.OnlyImportedAssets);*/
+            var displayOptionsField = rootVisualElement.Q<EnumFlagsField>(DISPLAY_OPTIONS);
+            displayOptionsField.Init((BrowserOptionsExtended)browserOptions);
+            displayOptionsField.RegisterValueChangedCallback(OnOptionsChanged);
+
+            FullDirectoryContents = null;
+            RefreshAdditionalOptions(BrowserOptionsExtended.None, (BrowserOptionsExtended)browserOptions);
         }
 
         public void ModifyAsset(VisualElement element, int i)
         {
             ListView directoryContent = GetDirectoryContent();
             var assetLocation = (IResourceLocation)directoryContent.itemsSource[i];
-            if (typeof(Shader).IsAssignableFrom(assetLocation.ResourceType) || typeof(SceneInstance).IsAssignableFrom(assetLocation.ResourceType))
+            var assetStatus = GetAssetStatus(assetLocation, out var assetRequest);
+            if (assetStatus == AssetStatus.NeverAvailable)
             {
                 return;
-            }
-            SerializableAssetRequest assetRequest = new SerializableAssetRequest { AssetLocation = assetLocation };
-            bool assetAlreadyRequested = false;
-            bool assetAlreadyExists = false;
-            if (AssetsOfRainManager.TryGetInstance(out var manager))
-            {
-                assetAlreadyRequested = manager.virtualAssets.ContainsVirtualAsset(assetRequest);
-                assetAlreadyExists = manager.virtualAssets.VirtualAssetExists(assetRequest);
             }
 
             VisualElement buttonPanel = element.Q(BUTTON_PANEL);
@@ -116,38 +124,38 @@ namespace AssetsOfRain.Editor.Browser
                 buttonPanel.Add(manageAssetButton);
             }
 
-            if (assetAlreadyExists)
+            switch (assetStatus)
             {
-                manageAssetButton.style.display = DisplayStyle.Flex;
-                manageAssetButton.style.backgroundColor = new StyleColor(new Color32(181, 56, 81, 255));
-                manageAssetButton.text = "Remove";
-                manageAssetButton.tooltip = "Remove this addressable asset from the project";
-                manageAssetButton.clickable = new Clickable(delegate ()
-                {
-                    var manager = AssetsOfRainManager.GetInstance();
-                    manager.virtualAssets.DeleteVirtualAsset(assetRequest);
-                    EditorUtility.SetDirty(manager);
-                    directoryContent?.RefreshItems();
-                });
-            }
-            else if (!assetAlreadyRequested)
-            {
-                manageAssetButton.style.display = DisplayStyle.Flex;
-                manageAssetButton.style.backgroundColor = new StyleColor(new Color32(67, 141, 168, 255));
-                manageAssetButton.text = "Import";
-                manageAssetButton.tooltip = "Add this addressable asset to the project";
-                manageAssetButton.clickable = new Clickable(delegate ()
-                {
-                    var manager = AssetsOfRainManager.GetInstance();
-                    manager.virtualAssets.ImportVirtualAsset(assetRequest);
-                    EditorUtility.SetDirty(manager);
-                    directoryContent?.RefreshItems();
-                });
-            }
-            else
-            {
-                manageAssetButton.style.display = DisplayStyle.None;
-                manageAssetButton.clickable = null;
+                case AssetStatus.Available:
+                    manageAssetButton.style.display = DisplayStyle.Flex;
+                    manageAssetButton.style.backgroundColor = new StyleColor(new Color32(49, 123, 150, 255));
+                    manageAssetButton.text = "Import";
+                    manageAssetButton.tooltip = "Add this addressable asset to the project";
+                    manageAssetButton.clickable = new Clickable(delegate ()
+                    {
+                        var manager = AssetsOfRainManager.GetInstance();
+                        manager.virtualAssets.ImportVirtualAsset(assetRequest);
+                        EditorUtility.SetDirty(manager);
+                        directoryContent?.RefreshItems();
+                    });
+                    break;
+                case AssetStatus.InProject:
+                    manageAssetButton.style.display = DisplayStyle.Flex;
+                    manageAssetButton.style.backgroundColor = new StyleColor(new Color32(181, 56, 81, 255));
+                    manageAssetButton.text = "Remove";
+                    manageAssetButton.tooltip = "Remove this addressable asset from the project";
+                    manageAssetButton.clickable = new Clickable(delegate ()
+                    {
+                        var manager = AssetsOfRainManager.GetInstance();
+                        manager.virtualAssets.DeleteVirtualAsset(assetRequest);
+                        EditorUtility.SetDirty(manager);
+                        directoryContent?.RefreshItems();
+                    });
+                    break;
+                case AssetStatus.Neutral:
+                    manageAssetButton.style.display = DisplayStyle.None;
+                    manageAssetButton.clickable = null;
+                    break;
             }
 
             VisualElement assetPanel = element.Q(ADDRESSABLE_ASSET);
@@ -170,7 +178,113 @@ namespace AssetsOfRain.Editor.Browser
             }
         }
 
+        public void OnOptionsChanged(ChangeEvent<Enum> evt)
+        {
+            RefreshAdditionalOptions((BrowserOptionsExtended)evt.previousValue, (BrowserOptionsExtended)browserOptions);
+        }
+
+        public void RefreshAdditionalOptions(BrowserOptionsExtended previousValue, BrowserOptionsExtended newValue)
+        {
+            BrowserOptionsExtended deltaValue = previousValue ^ newValue;
+            if ((deltaValue & BrowserOptionsExtended.NewOptions) == BrowserOptionsExtended.None)
+            {
+                return;
+            }
+            Debug.Log($"Options changed");
+            bool inProject = (newValue & BrowserOptionsExtended.InProject) > BrowserOptionsExtended.None;
+            bool available = (newValue & BrowserOptionsExtended.Available) > BrowserOptionsExtended.None;
+            if (!inProject && !available)
+            {
+                if (FullDirectoryContents != null)
+                {
+                    DirectoryContentsWrapper = FullDirectoryContents;
+                    FullDirectoryContents = null;
+                }
+            }
+            else
+            {
+                FullDirectoryContents ??= DirectoryContentsWrapper;
+                var filteredDirectoryContents = FullDirectoryContents
+                    .Select(Filter)
+                    .Where(x => x.Value != null && x.Value.Count > 0);
+
+                DirectoryContentsWrapper = new Dictionary<string, List<IResourceLocation>>(filteredDirectoryContents);
+
+                KeyValuePair<string, List<IResourceLocation>> Filter(KeyValuePair<string, List<IResourceLocation>> keyValuePair)
+                {
+                    var filteredResourceLocations = new List<IResourceLocation>(keyValuePair.Value.Where(IncludeAssetLocation));
+                    return new KeyValuePair<string, List<IResourceLocation>>(keyValuePair.Key, filteredResourceLocations);
+                }
+
+                bool IncludeAssetLocation(IResourceLocation assetLocation) => GetAssetStatus(assetLocation, out _) switch
+                {
+                    AssetStatus.InProject => inProject,
+                    AssetStatus.Available => available,
+                    _ => false,
+                };
+            }
+
+            IResourceLocation g = GetDirectory().selectedItems.OfType<IResourceLocation>().FirstOrDefault();
+            if (g != null)
+            {
+                string selected = GroupLocationWrapper(g);
+                EditorApplication.update += ReturnDirectorySelection;
+
+                void ReturnDirectorySelection()
+                {
+                    EditorApplication.update -= ReturnDirectorySelection;
+                    var directory = GetDirectory();
+                    for (int i = 0; i < directory.itemsSource.Count; i++)
+                    {
+                        if (directory.itemsSource[i] is IResourceLocation item && GroupLocationWrapper(item) == selected)
+                        {
+                            directory.SetSelection(i);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        public AssetStatus GetAssetStatus(IResourceLocation assetLocation, out SerializableAssetRequest assetRequest)
+        {
+            if (typeof(Shader).IsAssignableFrom(assetLocation.ResourceType) || typeof(SceneInstance).IsAssignableFrom(assetLocation.ResourceType))
+            {
+                assetRequest = default;
+                return AssetStatus.NeverAvailable;
+            }
+            assetRequest = new SerializableAssetRequest { AssetLocation = assetLocation };
+            bool assetAlreadyRequested = false;
+            bool assetAlreadyExists = false;
+            if (AssetsOfRainManager.TryGetInstance(out var manager))
+            {
+                assetAlreadyRequested = manager.virtualAssets.ContainsVirtualAsset(assetRequest);
+                assetAlreadyExists = manager.virtualAssets.VirtualAssetExists(assetRequest);
+            }
+            if (assetAlreadyExists)
+            {
+                return AssetStatus.InProject;
+            }
+            else if (!assetAlreadyRequested)
+            {
+                return AssetStatus.Available;
+            }
+            else
+            {
+                return AssetStatus.Neutral;
+            }
+        }
+
         public ListView GetDirectoryContent() => (ListView)directoryContentField.GetValue(this);
+        public ListView GetDirectory() => (ListView)directoryField.GetValue(this);
+
+        public string GroupLocationWrapper(IResourceLocation g) => (string)GroupLocation.Invoke(this, new object[] { g });
+
+        public Dictionary<string, List<IResourceLocation>> DirectoryContentsWrapper 
+        {
+            get => (Dictionary<string, List<IResourceLocation>>)DirectoryContentsField.GetValue(this);
+            set => DirectoryContentsField.SetValue(this, value);
+        }
     }
 }
 #endif
