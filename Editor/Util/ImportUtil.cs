23bcf82e124df147b36fd86e734821f3adbb18a9
Groove Salad <95263024+Priscillalala@users.noreply.github.com>
2025-01-03 12:58:08 -0800

    prepare util scripts for release

--- Editor/Util/ImportUtil.cs
+++ Editor/Util/ImportUtil.cs
@@ -1,30 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using UnityEditor;
+﻿using UnityEditor;
 using UnityEngine;
-using UnityEngine.AddressableAssets.ResourceLocators;
-using UnityEngine.ResourceManagement;
-using UnityEngine.ResourceManagement.AsyncOperations;
-using UnityEngine.ResourceManagement.ResourceLocations;
 using Object = UnityEngine.Object;
 
 namespace AssetsOfRain.Editor.Util
 {
     public static class ImportUtil
     {
-        public static int FindScriptInstanceID(Type classType)
-        {
-            MonoScript monoScript = AssetDatabase.FindAssets($"t:{nameof(MonoScript)}")
-                .Select(AssetDatabase.GUIDToAssetPath)
-                .Distinct()
-                .SelectMany(AssetDatabase.LoadAllAssetsAtPath)
-                .OfType<MonoScript>()
-                .FirstOrDefault(x => x.GetClass() == classType);
-            return monoScript ? monoScript.GetInstanceID() : 0;
-        }
-
         public static void SetScriptReference(Object asset, MonoScript monoScript)
         {
             const string SCRIPT_PROPERTY = "m_Script";
@@ -41,10 +22,9 @@ namespace AssetsOfRain.Editor.Util
 
         public static Texture2D DuplicateCompressedTexture(Texture2D srcTex)
         {
-            RenderTexture renderTex = RenderTexture.GetTemporary(srcTex.width, srcTex.height);//, 0, RenderTextureFormat.Default, RenderTextureReadWrite.Default);
+            RenderTexture renderTex = RenderTexture.GetTemporary(srcTex.width, srcTex.height);
             RenderTexture previous = RenderTexture.active;
             Graphics.Blit(srcTex, renderTex);
-            //RenderTexture.active = renderTex;
             Texture2D outputTex = new Texture2D(srcTex.width, srcTex.height);
             outputTex.ReadPixels(new Rect(0, 0, renderTex.width, renderTex.height), 0, 0);
             outputTex.Apply(false, true);
--- Editor/Util/ResourceLocatorExtensions.cs
+++ Editor/Util/ResourceLocatorExtensions.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Reflection;
 using UnityEngine.AddressableAssets.ResourceLocators;
-using UnityEngine.ResourceManagement;
-using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.ResourceManagement.ResourceLocations;
 
 namespace AssetsOfRain.Editor.Util
