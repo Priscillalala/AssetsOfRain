0d350d39b3cd75d5ee3bf35124b674e109268dc0
Groove Salad <95263024+Priscillalala@users.noreply.github.com>
2025-01-02 23:14:32 -0800

    add import extension

---
+++ Editor/ImportExtensions/RefreshAssetsOfRain.cs
@@ -0,0 +1,23 @@
+﻿using ThunderKit.Common;
+using ThunderKit.Core.Config;
+
+[assembly:ImportExtensions]
+
+namespace AssetsOfRain.Editor.ImportExtensions
+{
+    public class RefreshAssetsOfRain : OptionalExecutor
+    {
+        public override int Priority => Constants.Priority.AddressableGraphicsImport - 100;
+        public override string Name => "Refresh Assets of Rain";
+        public override string Description => "Refreshes all addressable assets created by Assets of Rain";
+
+        public override bool Execute()
+        {
+            if (AssetsOfRainManager.TryGetInstance(out var manager))
+            {
+                manager.RefreshVirtualAssets();
+            }
+            return true;
+        }
+    }
+}
